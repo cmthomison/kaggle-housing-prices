34658e1c2bc8e6b45d2c6e9854a9623fed2b73f3
Claire Thomison <cmthomison@gmail.com>
2022-09-18 13:14:22 -0500

    EDA script started.

---
+++ eda/eda.py
@@ -0,0 +1,14 @@
+"""
+Exploratory data analysis for Kaggle housing prices.
+"""
+
+import sys
+import os
+from pathlib import Path
+import pandas
+import pandas_profiling
+
+sys.path.append()
+from ...support import data_functions
+
+Path.cwd()
