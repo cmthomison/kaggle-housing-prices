6cd74aeaff0c759f814a25bafa251fe6b5476365
Claire Thomison <cmthomison@gmail.com>
2022-09-18 15:39:38 -0500

    EDA started; function to get column type lists.

--- eda/eda.py
+++ eda/eda.py
@@ -6,9 +6,28 @@ import sys
 import os
 from pathlib import Path
 import pandas
-import pandas_profiling
+import seaborn as sns
 
-sys.path.append()
-from ...support import data_functions
+sys.path.append('..')
+from support import data_functions as df
 
-Path.cwd()
+
+# Load data.
+data = df.load_train()
+
+# Ran into several errors testing out pandas profiling, so going back to
+# manual visualizations.
+
+# What I'm looking for:
+# [ ] Distribution of dependent variable (housing prices)
+# [ ] Distribution of independent variables
+# [ ] Correlation of independent variables with dependent variable
+# [ ] Missing values in independent variables
+# [ ] Ideas for feature engineering
+
+# Distribution of housing prices
+sns.histplot(data, x='SalePrice')
+# Right-skewed- a long tail for the fancy houses.
+
+# Distribution of independent variables.
+num_cols, text_cols = df.col_types(data)
--- support/data_functions.py
+++ support/data_functions.py
@@ -1,13 +1,27 @@
-    """
-    Functions used throughout the process to load/manipulate/push data.
-    """
+"""
+Functions used throughout the process to load/manipulate/push data.
+"""
 
-    import pandas as pd
-    import os
+import pandas as pd
+import os
 
 
-    def load_train(path='~/Documents/projects/kaggle-housing-prices/data/train.csv')
-        
-        data = pd.read_csv(path)
+def load_train(path='~/Documents/projects/kaggle-housing-prices/data/train.csv'):
+    
+    data = pd.read_csv(path)
 
-        return data
+    return data
+
+def col_types(df:pd.DataFrame):
+
+    # Numeric
+    numerics = ['int16', 'int32', 'int64', 'float16', 'float32', 'float64']
+
+    num_cols = df.select_dtypes(include=numerics).columns.tolist()
+
+    # String
+    strings = ['object', 'string']
+
+    string_cols = df.select_dtypes(include=strings).columns.tolist()
+
+    return num_cols, string_cols
