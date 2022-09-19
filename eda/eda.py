151d640b28edb4ae76411af747905c16c017e3d7
Claire Thomison <cmthomison@gmail.com>
2022-09-19 08:49:01 -0500

    Most of features categorized into thematic groups.

--- eda/eda.py
+++ eda/eda.py
@@ -31,3 +31,90 @@ sns.histplot(data, x='SalePrice')
 
 # Distribution of independent variables.
 num_cols, text_cols = df.col_types(data)
+
+# I'm also going to manually categorize the features into a few thematic
+# groups: size, age, quality, amenities, and sale details
+size = [
+    'BsmtFinSF1',
+    'BsmtFinSF2',
+    'BsmtUnfSF',
+    'TotalBsmtSF',
+    '1stFlrSF',
+    '2ndFlrSF',
+    'LowQualFinSF',
+    'WoodDeckSF',
+    'OpenPorchSF',
+    'LotArea',
+    'LotFrontage',
+    'MasVnrArea', 
+    'GrLivArea', 
+    'GarageArea',
+    'GarageCars',
+    'PoolArea',
+    'BedroomAbvGr',
+    'KitchenAbvGr',
+    'TotRmsAbvGrd'
+]
+
+age = [
+    'YearBuilt',
+    'YearRemodAdd'
+]
+
+quality = [
+    'OverallQual',
+    'OverallCond',
+    'KitchenQual',
+    'GarageQual',
+    'GarageCond',
+    'HeatingQC',
+    'PoolQC',
+    'Condition1',
+    'Condition2',
+    'ExterQual',
+    'ExterCond',
+    'BsmtQual',
+    'BmstCond'
+]
+
+amenities = [
+    'Heating',
+    'CentralAir',
+    'Electrical',
+    'GarageType',
+    'Fence',
+    'MiscFeature',
+    'EnclosedPorch',
+    '3SsnPorch',
+    'ScreenPorch',
+    'Fireplaces'
+]
+
+attributes = [
+    'MSZoning',
+    'Street',
+    'LotShape',
+    'LandContour',
+    'Utilities',
+    'LotConfig',
+    'LandSlope',
+    'Neighborhood',
+    'BldgType',
+    'HouseStyle',
+    'RoofStyle',
+    'RoofMatl',
+    'Exterior1st',
+    'Exterior2nd',
+    'MasVnrType',
+    'Foundation'
+]
+
+sale_deets = [
+    'SaleType',
+    'SaleCondition',
+    'MoSold',
+    'YrSold'
+]
+
+cat_cols = size + age + quality + amenities + attributes + sale_deets
+to_categorize = [x for x in data.columns.tolist() if x not in cat_cols]
