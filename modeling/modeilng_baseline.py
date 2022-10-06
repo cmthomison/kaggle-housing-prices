eebee294ff653813388a1cf1b92eebac4d4384b7
Claire Thomison <cmthomison@gmail.com>
2022-10-06 07:23:13 -0500

    Pipeline planning for base model.

---
+++ modeling/modeilng_baseline.py
@@ -0,0 +1,8 @@
+""" 
+Script to generate our baseline model where we'll use total square footage
+to predict SalePrice with linear regression.
+
+Example of a tranformation pipeline here: https://www.kaggle.com/code/fk0728/feature-engineering-with-sklearn-pipelines/notebook
+https://adamnovotny.com/blog/custom-scikit-learn-pipeline.html
+"""
+
