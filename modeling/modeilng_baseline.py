9380a4ba9cb339146db82ef94f4c7e780e78cd85
Claire Thomison <cmthomison@gmail.com>
2022-10-10 08:44:03 -0500

    Baseline model pipeline complete; next step- run test data through pipeline and generate submission.

--- modeling/modeilng_baseline.py
+++ modeling/modeilng_baseline.py
@@ -11,7 +11,10 @@ import datetime
 import multiprocessing
 import pandas as pd
 import numpy as np
+import datetime as dt
+from sklearn import linear_model
 from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 from sklearn.linear_model import LinearRegression
 from sklearn import metrics
@@ -54,18 +57,72 @@ class FeatTotalSf(BaseEstimator, TransformerMixin):
     
     def transform(self, X, y=None):
         sf_cols = ['1stFlrSF', '2ndFlrSF', 'TotalBsmtSF']
+
+        # Fill nulls (within the function to retain column names).
+        X[sf_cols] = X[sf_cols].fillna(0)
+
+        # Sum to get total square footage.
         X.loc[:, "sf_total"] = X[sf_cols].sum(axis=1)
         return X
 
-# PICK UP HERE
-# - Select SF features
-# - Use simple imputer strategy='constant' (0 fill) for any missing
-# values
-# Then train a linear regression model on the split
+# Create a pipeline for the baseline model.
+numeric_pipeline = Pipeline(steps=[
+    ('impute', SimpleImputer(strategy='constant'))
+])
+
+col_processor = ColumnTransformer(transformers=[
+    ('number', numeric_pipeline, ['1stFlrSF', '2ndFlrSF','TotalBsmtSF'])
+])
 
 test_pipeline = Pipeline(steps=[
     ("fe_squarefootage", FeatTotalSf()),
     ("feature_selection", FeatureSelector(['sf_total']))
 ])
 
-test = test_pipeline.fit_transform(data)
+preprocessing = Pipeline(steps=[
+    ("fe_squarefootage", FeatTotalSf()),
+    ("feature_selection", FeatureSelector(['sf_total']))
+])
+
+linear_regression = LinearRegression()
+
+lr_pipeline = Pipeline(steps=[
+    ("preprocessing", preprocessing),
+    ("modeling", linear_regression)
+])
+
+lr_model = lr_pipeline.fit(X_train, y_train)
+y_predict = lr_model.predict(X_valid)
+
+# Score the model.
+r2 = metrics.r2_score(y_valid, y_predict)
+mse = metrics.mean_squared_error(y_valid, y_predict)
+rmse = metrics.mean_squared_error(y_valid, y_predict, squared=False)
+rmsle = metrics.mean_squared_log_error(y_valid, y_predict, squared=False)
+
+print(f'R2: {r2}')
+print(f'MSE: {mse}')
+print(f'RMSE: {rmse}')
+print(f'RMSLE: {rmsle}')
+
+# Export scores.
+export = {
+    'model': 'Baseline',
+    'algorithm': 'Linear Regression',
+    'date_completed': dt.datetime.now(),
+    'r2': r2,
+    'mse': mse,
+    'rmse': rmse,
+    'rmsle': rmsle
+}
+
+pd.DataFrame(export, index=[0]).to_csv('~/Documents/projects/kaggle-housing-prices/modeling/results/modeling_results.csv', index=False)
+
+# Retrain the model on the full data before submitting a prediction.
+X = data.drop(columns='SalePrice')
+y = data[['SalePrice']]
+
+full_lr_model = lr_pipeline.fit(X, y)
+
+# Next steps:
+# - Run test data through pipeline to generate prediction for submission.
